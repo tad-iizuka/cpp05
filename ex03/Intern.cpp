05d8b3ffd305381329665046a95a9016f1e937ec
tad.iizuka@gmail.com <tad-iizuka>
2025-08-15 06:53:58 +0900

    change: log

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,24 +6,24 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 20:48:25 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/08/15 02:33:26 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/08/15 06:53:27 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 
 AForm*	genShrubberyCreationForm(const std::string& target) {
-	Log::a(F, L, C_G, "[" + target + "] generated.");
+	Log::a(F, L, C_G, "Intern creates " + target);
 	return new ShrubberyCreationForm(target);
 }
 
 AForm*	genRobotomyRequestForm(const std::string& target) {
-	Log::a(F, L, C_G, "[" + target + "] generated.");
+	Log::a(F, L, C_G, "Intern creates " + target);
 	return new RobotomyRequestForm(target);
 }
 
 AForm*	genPresidentialPardonForm(const std::string& target) {
-	Log::a(F, L, C_G, "[" + target + "] generated.");
+	Log::a(F, L, C_G, "Intern creates " + target);
 	return new PresidentialPardonForm(target);
 }
 
@@ -43,7 +43,7 @@ AForm*	Intern::makeForm(const std::string& form, const std::string& target) {
 			return (*table[i].f)(target);
 		}
 	}
-	Log::a(F, L, C_R, "[" + form + "] not registered.");
+	Log::a(F, L, C_R, "[" + form + "] form not registered.");
 	return NULL;
 }
 
