be0d3010bf923c6e131d43756ca883ca86e64b63
tad.iizuka@gmail.com <tad-iizuka>
2025-08-17 04:00:02 +0900

    change: level

--- ex03/PresidentialPardonForm.hpp
+++ ex03/PresidentialPardonForm.hpp
@@ -6,7 +6,7 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 14:30:13 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/08/14 18:22:49 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/08/17 03:57:26 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ class PresidentialPardonForm : public AForm
 
 	private:
 
-		static const int DEFAULT_SIGN_GRADE = 145;
-		static const int DEFAULT_EXEC_GRADE = 137;
+		static const int DEFAULT_SIGN_GRADE = 25;
+		static const int DEFAULT_EXEC_GRADE = 5;
 
 		const std::string _target;
 };
